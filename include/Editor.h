a292b6f4700e98e8b57db622f8761bc30685fdc2
hexaredecimal <mfanakagama@gmail.com>
2026-05-14 08:33:26 +0200

    Editor: Track the lines and columns on the cursor

--- include/Editor.h
+++ include/Editor.h
@@ -35,10 +35,24 @@ public:
     Rectangle get_cursor_rect() { return m_rect; }
     Vector2 get_cursor_position() { return { m_rect.x, m_rect.y }; }
 
+    int get_line() { return m_line; }
+    int get_column() { return m_col; }
+
+    bool get_blink() { return m_blink;}
+    bool set_blink(bool blink) { m_blink = blink; }
+
     void move_forward(int size) {
+
+        if (m_rect.x + size + 2 > m_content_rect.width)
+          if (m_rect.y + m_line_height > m_content_rect.height + 2)
+              return;
+
+
         m_rect.x += size + 2;
-        if (m_rect.x > m_content_rect.width)
+        m_col++;
+        if (m_rect.x > m_content_rect.width) {
             move_down();
+        }
     }
 
     void move_back(int size) {
@@ -46,19 +60,44 @@ public:
             return;
 
         m_rect.x -= size - 2;
+        m_col--;
     }
 
     void move_down() {
-        if (m_rect.y + m_line_height > m_content_rect.height + 2)
+        if (m_rect.y + m_line_height > m_content_rect.height + m_line_height)
             return;
         m_rect.y += m_line_height;
         m_rect.x = m_content_rect.x + 2;
+        m_line++;
+        m_col = 1;
     }
 
+
+    void move_up() {
+        if (m_rect.y - m_line_height < m_content_rect.y - m_line_height)
+            return;
+        m_rect.y -= m_line_height;
+        m_rect.x = m_content_rect.x + 2;
+        m_line--;
+    }
+
+    void update(){
+        if (!m_blink) return;
+        m_timer--;
+        if (m_timer < -35.0f) {
+            m_timer = 35.0f;
+        }
+    }
+
+    float get_timer() { return m_timer; }
 private:
     Rectangle m_rect;
     Rectangle m_content_rect;
     int m_line_height;
+    int m_line = 1;
+    int m_col = 1;
+    bool m_blink = false;
+    float m_timer = 35.0f;
 };
 
 struct Editor {
@@ -80,7 +119,28 @@ public:
     EditorMode get_editor_mode() { return m_mode; }
     void set_editor_mode(EditorMode mode) { m_mode = mode; }
 
-    Cursor get_cursor() { return m_cursor; }
+    Cursor* get_cursor() { return &m_cursor; }
+    void update() {
+      int dummy_size = MeasureText("\t", 14);
+      m_cursor.update();
+
+      if (IsKeyPressed(KEY_RIGHT)) {
+        m_cursor.move_forward(dummy_size);
+      }
+
+      if (IsKeyPressed(KEY_LEFT)) {
+        m_cursor.move_back(dummy_size);
+      }
+
+      if (IsKeyPressed(KEY_UP)) {
+        m_cursor.move_up();
+      }
+
+      if (IsKeyPressed(KEY_DOWN)) {
+        m_cursor.move_down();
+      }
+
+    }
 private:
     Cursor m_cursor;
     EditorMode m_mode;
