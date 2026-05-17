a26bd1990a17ace3de5b5942f2ac5c13516b5d49
hexaredecimal <mfanakagama@gmail.com>
2026-05-17 20:55:25 +0200

    CommonSyntax: Added the python syntax highlighter

--- include/CommonSyntax.h
+++ include/CommonSyntax.h
@@ -10,6 +10,7 @@ public:
 
 private:
     static Highlighter c_cpp();
+    static Highlighter py();
 
     static std::vector<Highlighter> MAP;
 };
--- src/CommonSyntax.cpp
+++ src/CommonSyntax.cpp
@@ -1,10 +1,14 @@
 #include <CommonSyntax.h>
+#include <iostream>
 
 std::vector<Highlighter> CommonSyntax::MAP{};
 
 void CommonSyntax::init() {
     Highlighter c_cpp_h = CommonSyntax::c_cpp();
     CommonSyntax::MAP.push_back(c_cpp_h);
+
+    Highlighter python = CommonSyntax::py();
+    CommonSyntax::MAP.push_back(python);
 }
 
 Highlighter* CommonSyntax::get_highligher(std::string& ext) {
@@ -20,6 +24,40 @@ Highlighter* CommonSyntax::get_highligher(std::string& ext) {
     return nullptr;
 }
 
+Highlighter CommonSyntax::py() {
+    Highlighter python;
+    python.add_extension(".py");
+
+    python.add_words({
+       "False","None","True","and","as","assert","async","await",
+        "break","class","continue","def","del","elif","else","except",
+        "finally","for","from","global","if","import",
+        "in","is","lambda","nonlocal","not","or","pass",
+        "raise","return","try","while","with","yield"}, BLUE);
+
+    python.set_highter_function([](std::vector<CharView>& m_chars, int *index_ptr) {
+      int index = *index_ptr;
+      CharView* cv = &m_chars[index];
+      char c = cv->get_char();
+
+      if (c == '#') {
+          Color color = ColorBrightness(GREEN, -0.5);
+          int line = cv->get_line();
+          for (int i = index; i < (int)m_chars.size(); i++) {
+              if (m_chars[i].get_line() > line) {
+                  index = i - 1;
+                  break;
+              }
+              m_chars[i].set_color(color);
+          }
+        return true;
+      }
+
+      return false;
+    });
+    return python;
+}
+
 Highlighter CommonSyntax::c_cpp() {
     Highlighter c_cpp_h;
     c_cpp_h.add_extension(".c");
@@ -29,13 +67,14 @@ Highlighter CommonSyntax::c_cpp() {
     c_cpp_h.add_extension(".cxx");
     c_cpp_h.add_extension(".hxx");
 
-    c_cpp_h.add_words({
-        "include", "if", "else", "define", "switch", "return", "for", "while", "do",
-        "auto", "class", "struct", "namespace", "using", "typedef",
-        "const", "constexpr", "enum", "union", "public", "private",
-        "protected", "pragma", "this", "inline", "break", "continue",
-        "goto", "static"
-    }, BLUE);
+    c_cpp_h.add_words(
+        {"include",   "if",     "else",      "define", "endif",    "extern",
+         "switch",    "return", "for",       "while",  "do",       "auto",
+         "class",     "struct", "namespace", "using",  "typedef",  "const",
+         "constexpr", "enum",   "union",     "public", "private",  "protected",
+         "pragma",    "this",   "inline",    "break",  "continue", "goto",
+         "static",    "ifndef", "ifdef"},
+        BLUE);
 
     c_cpp_h.add_words({
         "int", "float", "double", "short", "long", "void", "bool",
@@ -63,17 +102,20 @@ Highlighter CommonSyntax::c_cpp() {
               }
               m_chars[i].set_color(color);
           }
+          return true;
       } else if (c == '/' && index + 1 < m_chars.size() && m_chars[index + 1].get_char() == '/') {
           Color color = ColorBrightness(GREEN, -0.5);
           int line = cv->get_line();
           for (int i = index; i < (int)m_chars.size(); i++) {
               if (m_chars[i].get_line() > line) {
-                  index = i;
-                  break;
+                *index_ptr = i - 1;
+                break;
               }
               m_chars[i].set_color(color);
           }
+          return true;
       }
+      return false;
     });
 
     return c_cpp_h;
