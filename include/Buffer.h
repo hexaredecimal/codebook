1b24710b27fe1508d7a033a0cc8a28f25641cee8
hexaredecimal <mfanakagama@gmail.com>
2026-05-17 20:54:44 +0200

    Buffer: Format with clangfmt and implement text selection ranges

--- include/Buffer.h
+++ include/Buffer.h
@@ -10,33 +10,35 @@
 
 class Buffer {
 public:
-    Buffer(std::string file_path, std::string file_content, int w, int h):
-        m_file_path{file_path},
-        m_file_contents{file_content},
-        m_file_name(file_path.size() == 0 ? "No file" : GetFileName(file_path.c_str())),
-        m_text_index(0),
-        m_start_line(1),
-        m_cursor{w,h},
-        m_highlighter{nullptr},
-        m_ext{".txt"},
-        m_is_dirty{false}
-    {}
+ Buffer(std::string file_path, std::string file_content, int w, int h)
+     : m_file_path{file_path},
+       m_file_contents{file_content},
+       m_file_name(file_path.size() == 0 ? "No file"
+                                         : GetFileName(file_path.c_str())),
+       m_text_index(0),
+       m_start_line(1),
+       m_cursor{w, h},
+       m_highlighter{nullptr},
+       m_ext{".txt"},
+       m_is_dirty{false},
+       m_text_selection_range{-1, -1},
+       m_prev_selection_range{-1, -1} {}
 
-    void next_char() { m_text_index++; }
-    void prev_char() { m_text_index--; }
-    int get_text_index() { return m_text_index; }
-    void set_text_index(int index) { m_text_index = index; }
+ void next_char() { m_text_index++; }
+ void prev_char() { m_text_index--; }
+ int get_text_index() { return m_text_index; }
+ void set_text_index(int index) { m_text_index = index; }
 
-    void set_start_line(int line) { m_start_line = line; }
-    int get_start_line() { return m_start_line; }
+ void set_start_line(int line) { m_start_line = line; }
+ int get_start_line() { return m_start_line; }
 
-    std::string get_file_contents() { return m_file_contents; }
-    void insert_char(char c) {
-      m_file_contents.insert(m_text_index, 1, c);
-      next_char();
-      if (!m_is_dirty)
-          m_is_dirty = true;
-    }
+ std::string get_file_contents() { return m_file_contents; }
+ void set_file_contents(std::string file) { m_file_contents = file; }
+ void insert_char(char c) {
+   m_file_contents.insert(m_text_index, 1, c);
+   next_char();
+   if (!m_is_dirty) m_is_dirty = true;
+ }
 
     void delete_char() {
       if (m_text_index > 0) {
@@ -68,7 +70,23 @@ public:
     void set_extension(std::string ext) { m_ext = ext; }
     std::string get_extension() { return m_ext; }
 
-private:
+    void set_selection(int start, int end) {
+      m_prev_selection_range.x = m_text_selection_range.x;
+      m_prev_selection_range.y = m_text_selection_range.y;
+      m_text_selection_range.x = start;
+      m_text_selection_range.y = end;
+    }
+
+    Vector2 get_last_selection() { return m_prev_selection_range; }
+    Vector2 get_selection() { return m_text_selection_range; }
+    void clear_selection() {
+      m_prev_selection_range.x = m_text_selection_range.x;
+      m_prev_selection_range.y = m_text_selection_range.y;
+      m_text_selection_range.x = -1;
+      m_text_selection_range.y = -1;
+    }
+
+   private:
     std::string m_file_path;
     std::string m_file_contents;
     std::string m_file_name;
@@ -78,6 +96,6 @@ private:
     int m_start_line = 1;
     Cursor m_cursor;
     bool m_is_dirty;
-
-
+    Vector2 m_text_selection_range;
+    Vector2 m_prev_selection_range;
 };
